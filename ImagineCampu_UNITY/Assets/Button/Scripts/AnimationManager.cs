b001ffcff86cf89e5e4896df672bee11f7c0dedc
Marcelo Gomes <marceloanselmogomes@gmail.com>
2016-11-11 16:05:15 -0200

    Menu v01 Pronto

---
+++ ImagineCampu_UNITY/Assets/Button/Scripts/AnimationManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Collider2D))]
+public class AnimationManager : MonoBehaviour {
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter() {
+		anim.SetBool ("mouseOver",true);
+	}
+
+	void OnMouseExit() {
+		anim.SetBool ("mouseOver",false);
+	}
+}
